bf717ecc54b5b8685aab43eea6a84a025834b17b
S4njuuu3291 <sanju329121@gmail.com>
2026-02-20 17:55:09 +0700

    simplify: remove dynamodb lock from backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "jobscraper-terraform-state-730335315755"
-    key            = "terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket = "jobscraper-terraform-state-730335315755"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+    encrypt = true
   }
 }
