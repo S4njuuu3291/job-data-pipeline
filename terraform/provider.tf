d885e0aac287345750b9b331ff4f13da70fbb86d
S4njuuu3291 <sanju329121@gmail.com>
2026-02-20 08:48:32 +0700

    fix ci

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,9 +13,6 @@ terraform {
 provider "aws" {
   region = var.aws_region
 
-  profile = "jobscraper-terraform-executor"
-
-  # Optionally allow skipping requesting account ID
   skip_metadata_api_check = true
   skip_region_validation  = false
 
