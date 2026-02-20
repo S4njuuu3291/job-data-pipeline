1f5b1fef459f66d5ab65813975b1c12333c401ed
S4njuuu3291 <sanju329121@gmail.com>
2026-02-20 09:47:34 +0700

    feat: update dependencies and add pandas-stubs for type annotations

--- src/utils/data_validator.py
+++ src/utils/data_validator.py
@@ -1,23 +1,26 @@
 import pandas as pd
 import pandera.pandas as pa
-from pandera.pandas import DataFrameSchema,Column,Check
+from pandera.pandas import DataFrameSchema, Column, Check
 import json
 
 job_schema = DataFrameSchema(
     {
-        "job_id": Column(str, unique= True),
+        "job_id": Column(str, unique=True),
         "job_title": Column(str),
         "company_name": Column(str),
         "location": Column(str),
         "job_url": Column(str),
-        "platform": Column(str, Check.isin(["kalibrr","jobstreet","glints","dealls","karir.com"])),
-        "scraped_at": Column(str)
+        "platform": Column(
+            str, Check.isin(["kalibrr", "jobstreet", "glints", "dealls", "karir.com"])
+        ),
+        "scraped_at": Column(str),
     },
     strict=False,
-    coerce=True
+    coerce=True,
 )
 
-def validate_job_data(df:pd.DataFrame):
+
+def validate_job_data(df: pd.DataFrame):
     try:
         df_validated = job_schema.validate(df)
         print("✅ Data berhasil divalidasi!")
@@ -26,18 +29,19 @@ def validate_job_data(df:pd.DataFrame):
         print(f"❌ Data kotor/tidak valid: {e}")
         raise e
 
+
 if __name__ == "__main__":
-    file_path = r"output/kalibrr_raw_20260216_081739.json" 
-    
+    file_path = r"output/kalibrr_raw_20260216_081739.json"
+
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            results = json.load(f) 
+        with open(file_path, "r", encoding="utf-8") as f:
+            results = json.load(f)
         print("✅ Berhasil ngeload JSON")
-        
+
         df = pd.DataFrame(results)
-        
+
         job_schema.validate(df)
         print("✅ Data berhasil divalidasi!")
-        
+
     except Exception as e:
         print(f"❌ Gagal proses: {e}")
